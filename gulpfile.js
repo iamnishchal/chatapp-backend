c25c59793c7a56e5293c7cd510e441fd234ea16e
Nishchal <nishchal.p@hashedin.com>
2020-05-16 14:01:14 +0530

    Server application for Angular chat app

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  livereload = require('gulp-livereload'),
+  ts = require('gulp-typescript');
+
+
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("build", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("./dist"));
+});
+
+gulp.task('default', gulp.series('build'))
+
+
+gulp.task('serve', function() {
+  gulp.watch('./**/*.ts');
+
+  livereload.listen();
+  nodemon({
+    exec: 'ts-node ./src/index.ts',
+    ext: 'ts'
+  }).on('restart', function() {
+    setTimeout(function() {
+      console.log('reload!');
+      livereload.reload();
+    }, 500);
+  });
+});
+
+gulp.task('watch', gulp.series('build', 'serve'))
